6332ffcde2f59e5ad18fd014c07715eead4504ec
zakixd12 <105186532+zakixd12@users.noreply.github.com>
2022-09-14 18:33:40 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -7,17 +7,9 @@ let handler = async (m, { conn, usedPrefix }) => {
       noteMessage: {
       extendedTextMessage: {
       text: `
-╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
-│ • Saweria  [https://saweria.co/arullofc]
-╰────
-╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+╭─「 Donasi • Gopay 」
+│ • Three  [089506219231]
+│ • Gopay  [089506219231]
 ╰────
 `,
       contextInfo: {
